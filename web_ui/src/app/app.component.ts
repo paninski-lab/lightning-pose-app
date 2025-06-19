7c73ba1ab91bd436b831154f3f575f60b959599c
Karan Sikka <karanssikka@gmail.com>
2025-06-19 19:14:53 -0400

    settings form is working

--- web_ui/src/app/app.component.ts
+++ web_ui/src/app/app.component.ts
@@ -1,6 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ProjectSettingsComponent } from './project-settings/project-settings.component';
+import { ProjectInfoService } from './project-info.service';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +23,22 @@ import { ProjectSettingsComponent } from './project-settings/project-settings.co
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  projectInfoService = inject(ProjectInfoService);
+
+  protected isIniting = signal(false);
+  protected settingsDialog = viewChild<ElementRef | undefined>(
+    'settingsDialog',
+  );
+
+  async ngOnInit() {
+    await this.projectInfoService.loadProjectInfo();
+    this.isIniting.set(true);
+  }
+
+  protected openSettingsDialog() {
+    const elementRef = this.settingsDialog();
+    if (!elementRef) return; // projectinfo not yet loaded.
+    (elementRef.nativeElement as HTMLDialogElement).showModal();
+  }
+}
--- web_ui/src/app/project-settings/project-settings.component.ts
+++ web_ui/src/app/project-settings/project-settings.component.ts
@@ -1,10 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  output,
+} from '@angular/core';
+import { ProjectInfoService } from '../project-info.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ProjectInfo } from '../project-info';
 
 @Component({
   selector: 'app-project-settings',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './project-settings.component.html',
   styleUrl: './project-settings.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectSettingsComponent {}
+export class ProjectSettingsComponent {
+  done = output<null>();
+
+  protected dataDir = new FormControl('');
+  protected modelDir = new FormControl('');
+  private projectInfoService = inject(ProjectInfoService);
+
+  ngOnInit() {
+    const projectInfo = this.projectInfoService.projectInfo;
+    this.dataDir.setValue(projectInfo.data_dir);
+    this.modelDir.setValue(projectInfo.model_dir);
+  }
+
+  protected async onSaveClick() {
+    // Save the project info.
+    const projectInfo = {} as Partial<ProjectInfo>;
+
+    if (this.dataDir.dirty) {
+      projectInfo.data_dir = this.dataDir.value ?? '';
+    }
+    if (this.modelDir.dirty) {
+      projectInfo.model_dir = this.modelDir.value ?? '';
+    }
+
+    if (Object.keys(projectInfo).length) {
+      await this.projectInfoService.setProjectInfo(projectInfo);
+    }
+
+    // If successful the service is going to reload the app because
+    // project info is global state.
+  }
+}
