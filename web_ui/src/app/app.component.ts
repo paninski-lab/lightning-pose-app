327f724568eb4d472b9b929e6085dce7dc3b5772
Karan Sikka <karanssikka@gmail.com>
2025-06-19 17:42:26 -0400

    Add settings modal styles

--- web_ui/src/app/app.component.ts
+++ web_ui/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ProjectSettingsComponent } from './project-settings/project-settings.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ProjectSettingsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ web_ui/src/app/project-settings/project-settings.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-settings',
+  imports: [],
+  templateUrl: './project-settings.component.html',
+  styleUrl: './project-settings.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectSettingsComponent {}
