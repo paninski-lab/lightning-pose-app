d4e2494d686219050102d40c7d7660bfd0319b7e
Karan Sikka <karanssikka@gmail.com>
2025-06-19 19:14:59 -0400

    black

--- app_server/src/litpose_app/super_rglob.py
+++ app_server/src/litpose_app/super_rglob.py
@@ -2,6 +2,7 @@ import datetime
 
 from wcmatch import pathlib as w
 
+
 def super_rglob(base_path, pattern=None, no_dirs=False, stat=False):
     """
     Needs to be performant when searching over large model directory.
@@ -13,7 +14,10 @@ def super_rglob(base_path, pattern=None, no_dirs=False, stat=False):
     flags = w.GLOBSTAR
     if no_dirs:
         flags |= w.NODIR
-    results = w.Path(base_path).glob(pattern, flags=flags, )
+    results = w.Path(base_path).glob(
+        pattern,
+        flags=flags,
+    )
     result_dicts = []
     for r in results:
         stat_info = r.stat() if stat else None
@@ -26,10 +30,18 @@ def super_rglob(base_path, pattern=None, no_dirs=False, stat=False):
             "type": "dir" if is_dir else "file" if is_dir == False else None,
             "size": stat_info.st_size if stat_info else None,
             # Note: st_birthtime is more reliable for creation time on some systems
-            "cTime": datetime.datetime.fromtimestamp(
-                getattr(stat_info, "st_birthtime", stat_info.st_ctime)
-            ).isoformat() if stat_info else None,
-            "mTime": datetime.datetime.fromtimestamp(stat_info.st_mtime).isoformat() if stat_info else None,
+            "cTime": (
+                datetime.datetime.fromtimestamp(
+                    getattr(stat_info, "st_birthtime", stat_info.st_ctime)
+                ).isoformat()
+                if stat_info
+                else None
+            ),
+            "mTime": (
+                datetime.datetime.fromtimestamp(stat_info.st_mtime).isoformat()
+                if stat_info
+                else None
+            ),
         }
 
         result_dicts.append(d)
