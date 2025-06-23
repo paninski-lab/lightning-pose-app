e2d9b6bf373141f3bd4b4f266050211e2cc2796f
Karan Sikka <karanssikka@gmail.com>
2025-06-23 17:57:42 -0400

    save button form dirty check

--- web_ui/src/app/project-settings/project-settings.component.ts
+++ web_ui/src/app/project-settings/project-settings.component.ts
@@ -8,7 +8,7 @@ import {
   signal,
 } from '@angular/core';
 import { ProjectInfoService } from '../project-info.service';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ProjectInfo } from '../project-info';
 
 @Component({
@@ -22,18 +22,27 @@ export class ProjectSettingsComponent implements OnInit {
   done = output<null>();
   setupMode = input(false);
 
-  protected dataDir = new FormControl('');
-  protected modelDir = new FormControl('');
-  protected views = new FormControl('');
+  protected projectInfoForm: FormGroup;
   protected viewsInitialRows = signal(4);
   private projectInfoService = inject(ProjectInfoService);
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.projectInfoForm = this.fb.group({
+      dataDir: [''],
+      modelDir: [''],
+      views: [''],
+    });
+  }
 
   ngOnInit() {
     const projectInfo = this.projectInfoService.projectInfo;
     if (projectInfo) {
-      this.dataDir.setValue(projectInfo.data_dir);
-      this.modelDir.setValue(projectInfo.model_dir);
-      this.views.setValue(projectInfo.views.join('\n'));
+      this.projectInfoForm.patchValue({
+        dataDir: projectInfo.data_dir,
+        modelDir: projectInfo.model_dir,
+        views: projectInfo.views.join('\n'),
+      });
       this.viewsInitialRows.update((x) =>
         Math.max(x, projectInfo.views.length),
       );
@@ -44,20 +53,12 @@ export class ProjectSettingsComponent implements OnInit {
     // Save the project info.
     const projectInfo = {} as Partial<ProjectInfo>;
 
-    if (this.dataDir.dirty) {
-      projectInfo.data_dir = this.dataDir.value ?? '';
-    }
-    if (this.modelDir.dirty) {
-      projectInfo.model_dir = this.modelDir.value ?? '';
-    }
-    if (this.views.dirty) {
-      projectInfo.views = this.parseMultilineText(this.views.value ?? '');
-    }
-
-    if (Object.keys(projectInfo).length) {
-      await this.projectInfoService.setProjectInfo(projectInfo);
-    }
-
+    projectInfo.data_dir = this.projectInfoForm.get('dataDir')?.value ?? '';
+    projectInfo.model_dir = this.projectInfoForm.get('modelDir')?.value ?? '';
+    projectInfo.views = this.parseMultilineText(
+      this.projectInfoForm.get('views')?.value ?? '',
+    );
+    await this.projectInfoService.setProjectInfo(projectInfo);
     // If successful the service is going to reload the app because
     // project info is global state.
   }
