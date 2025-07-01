572efde8039d9a9d9b87c7adac57233e052c182b
Karan Sikka <karanssikka@gmail.com>
2025-07-01 19:10:23 -0400

    Count the tasks correctly this time

--- app_server/src/litpose_app/tasks/management.py
+++ app_server/src/litpose_app/tasks/management.py
@@ -9,12 +9,25 @@ def setup_active_task_registry(app: FastAPI):
     # Get APScheduler instance
     scheduler: BaseScheduler = app.state.scheduler
 
-    subject = reactivex.subject.BehaviorSubject(len(scheduler.get_jobs()))
+    subject = reactivex.subject.BehaviorSubject(0)
 
     app.state.num_active_transcode_tasks = subject
 
-    def my_listener(event):
-        jobs = scheduler.get_jobs()
-        subject.on_next(len(jobs))
+    _num_active_jobs = 0
 
-    scheduler.add_listener(my_listener, e.EVENT_JOB_ADDED | e.EVENT_JOB_REMOVED)
+    def my_listener(event):
+        nonlocal _num_active_jobs
+        if event.code == e.EVENT_JOB_SUBMITTED:
+            _num_active_jobs += 1
+        else:
+            _num_active_jobs -= 1
+
+        subject.on_next(_num_active_jobs)
+
+    scheduler.add_listener(
+        my_listener,
+        e.EVENT_JOB_SUBMITTED
+        | e.EVENT_JOB_EXECUTED
+        | e.EVENT_JOB_ERROR
+        | e.EVENT_JOB_MISSED,
+    )
