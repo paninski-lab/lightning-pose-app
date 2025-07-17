5cd14cd71c59854641ea40927962bae88df9d445
Karan Sikka <karanssikka@gmail.com>
2025-07-17 16:55:50 -0400

    multiview parsing

--- web_ui/src/app/labeler/labeler-page.component.ts
+++ web_ui/src/app/labeler/labeler-page.component.ts
@@ -20,7 +20,7 @@ import { HttpClient } from '@angular/common/http';
 import { CsvParserService } from '../csv-parser.service';
 import * as dfd from 'danfojs';
 import { LKeypoint, SaveActionData } from './types';
-import { MVFrame } from './frame.model';
+import { FrameView, MVFrame } from './frame.model';
 import { Pair } from '../utils/pair';
 
 @Component({
@@ -99,19 +99,41 @@ export class LabelerPageComponent implements OnInit, OnChanges {
 
     try {
       this.isLoading.set(true);
-      // Use the first view's CSV path
-      const csvPath = labelFile.views[0].csvPath;
-      // Fetch the CSV file
-      const csvString = await this.fetchCsvFile(csvPath);
-      if (!csvString) {
-        console.error('Failed to load CSV file:', csvPath);
-        return;
-      }
 
-      // Parse the CSV file
-      const df = this.csvParser.parsePredictionFile(csvString);
-      this.labelFileData.set(df);
-      this.allFrames.set(this.parseDfToFMVFrame(df));
+      // For each view, fetch and process CSV files.
+      // Store output here: (initialize to preset the key insertion order)
+      const parsedDfsPerView = Object.fromEntries(
+        labelFile.views.map(
+          ({ viewName }): [string, dfd.DataFrame | undefined] => [
+            viewName,
+            undefined,
+          ],
+        ),
+      );
+      // Store promises here so we can wait on them:
+      const csvRequests = labelFile.views.map(async ({ viewName, csvPath }) => {
+        const csvString = await this.fetchCsvFile(csvPath);
+        if (!csvString) {
+          console.error('Failed to load CSV file:', csvPath);
+          return;
+        }
+        // Parse the CSV file
+        const df = this.csvParser.parsePredictionFile(csvString);
+        parsedDfsPerView[viewName] = df;
+        return;
+      });
+
+      // Sort the dfs by order of the views in the label file.
+
+      await Promise.all(csvRequests);
+
+      // Narrow the type by filtering out undefined (label files that failed to load).
+      const y = Object.entries(parsedDfsPerView).filter(
+        ([_, df]) => df !== undefined,
+      ) as [string, dfd.DataFrame][];
+      const z = Object.fromEntries(y);
+      const mvFrames = this.parseDfToMVFrame(z);
+      this.allFrames.set(mvFrames);
     } catch (error) {
       console.error('Error loading label file data:', error);
       this.labelFileData.set(null);
@@ -134,55 +156,85 @@ export class LabelerPageComponent implements OnInit, OnChanges {
       return null;
     }
   }
+  private parseKPsOfDfRow(
+    labelFileData: dfd.DataFrame,
+    currIndex: string,
+  ): LKeypoint[] {
+    const columns = labelFileData.columns.map(Pair.fromMapKey) as Pair<
+      string,
+      string
+    >[];
 
-  private parseDfToFMVFrame(labelFileData: dfd.DataFrame): MVFrame[] {
+    const groupedColumns = columns.reduce(
+      (acc, column) => {
+        const bodyPart = column.first;
+        if (!acc[bodyPart]) {
+          acc[bodyPart] = [];
+        }
+        acc[bodyPart].push(column);
+        return acc;
+      },
+      {} as Record<string, Pair<string, string>[]>,
+    );
+
+    const kps: LKeypoint[] = Object.entries(groupedColumns)
+      .map(([keypointName, cols]) => {
+        const xColumn = cols.find((col) => col.second === 'x');
+        const yColumn = cols.find((col) => col.second === 'y');
+
+        const x = xColumn
+          ? (labelFileData.at(currIndex, xColumn.toMapKey()) as number)
+          : null;
+        const y = yColumn
+          ? (labelFileData.at(currIndex, yColumn.toMapKey()) as number)
+          : null;
+
+        if (x == null || y == null) return null;
+        return { keypointName, x, y };
+      })
+      .filter((x) => x != null);
+
+    return kps;
+  }
+
+  private parseDfToMVFrame(dfs: Record<string, dfd.DataFrame>): MVFrame[] {
+    const framesPerView = {} as Record<string, FrameView[]>;
+
+    Object.entries(dfs).forEach(([viewName, df]) => {
+      framesPerView[viewName] = this.parseDfToFrameView(viewName, df);
+    });
+
+    const maxRowCount = Math.max(
+      ...Object.values(framesPerView).map((frames) => frames.length),
+    );
+
+    const mvFrames = Array.from({ length: maxRowCount }).map(
+      (_, i): MVFrame => {
+        const views = Object.values(framesPerView)
+          .map((frameViews) => frameViews[i])
+          .filter((x) => x != null);
+        const key = views[0].imgPath; // todo group by view.
+        return {
+          key,
+          views,
+        };
+      },
+    );
+    return mvFrames;
+  }
+
+  private parseDfToFrameView(
+    viewName: string,
+    labelFileData: dfd.DataFrame,
+  ): FrameView[] {
     // map each row of the label file to an MVFrame.
-    return labelFileData.index.map((imgPath): MVFrame => {
+    return labelFileData.index.map((imgPath): FrameView => {
       imgPath = imgPath as string;
 
-      const columns = labelFileData.columns.map(Pair.fromMapKey) as Pair<
-        string,
-        string
-      >[];
-
-      const groupedColumns = columns.reduce(
-        (acc, column) => {
-          const bodyPart = column.first;
-          if (!acc[bodyPart]) {
-            acc[bodyPart] = [];
-          }
-          acc[bodyPart].push(column);
-          return acc;
-        },
-        {} as Record<string, Pair<string, string>[]>,
-      );
-
-      const kps: LKeypoint[] = Object.entries(groupedColumns)
-        .map(([keypointName, cols]) => {
-          const xColumn = cols.find((col) => col.second === 'x');
-          const yColumn = cols.find((col) => col.second === 'y');
-
-          const x = xColumn
-            ? (labelFileData.at(imgPath, xColumn.toMapKey()) as number)
-            : null;
-          const y = yColumn
-            ? (labelFileData.at(imgPath, yColumn.toMapKey()) as number)
-            : null;
-
-          if (x == null || y == null) return null;
-          return { keypointName, x, y };
-        })
-        .filter((x) => x != null);
-
       return {
-        key: imgPath, // todo extract out view,
-        views: [
-          {
-            viewName: 'top',
-            imgPath,
-            keypoints: kps,
-          },
-        ],
+        viewName,
+        imgPath,
+        keypoints: this.parseKPsOfDfRow(labelFileData, imgPath),
       };
     });
   }
