b14ebd55a6d4fe39f1e85308a4b0114ff7dc8fce
Karan Sikka <karanssikka@gmail.com>
2025-08-18 17:38:41 -0400

    extract frames dialog interactions

--- web_ui/src/app/labeler/extract-frames-dialog/extract-frames-dialog.component.ts
+++ web_ui/src/app/labeler/extract-frames-dialog/extract-frames-dialog.component.ts
@@ -1,23 +1,25 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   inject,
   signal,
 } from '@angular/core';
 import { DialogRef } from '@angular/cdk/dialog';
 import { ViewerSessionsPanelComponent } from '../../viewer/viewer-left-panel/viewer-sessions-panel.component';
 import { Session } from '../../session.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-extract-frames-dialog',
-  imports: [ViewerSessionsPanelComponent],
+  imports: [ViewerSessionsPanelComponent, FormsModule],
   templateUrl: './extract-frames-dialog.component.html',
   styleUrl: './extract-frames-dialog.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExtractFramesDialogComponent {
   private dialogRef = inject(DialogRef);
-  protected step = 'session';
+  protected step = signal('session');
 
   // Form data
   protected session = signal<Session | null>(null);
@@ -27,28 +29,27 @@ export class ExtractFramesDialogComponent {
     this.dialogRef.close();
   }
 
-  protected handleStepClick(event: Event, step: string) {
-    event.preventDefault();
-    this.step = step;
-  }
-
   handleSelectedSessionChange(session: Session | null) {
     this.session.set(session);
   }
 
-  /** Returns true if the form is valid: session is selected, options are valid. */
-  protected isValid() {
-    if (this.session() === null) {
-      return false;
-    }
+  protected sessionStepIsValid = computed(() => {
+    return this.session() !== null;
+  });
+
+  protected settingsStepIsValid = computed(() => {
     if (this.nFrames() === null || this.nFrames()! < 1) {
       return false;
     }
     return true;
-  }
+  });
 
   /** Makes a request object that the extract frames service can execute. */
   private toExtractFramesRequest() {
     // Assume validity. Calls will be guarded by is valid.
+  }
+
+  handleExtractFramesClick() {
+    //todo
   }
 }
