c778bdb2d2db1e0ff6f6c6715a074e8e09f035a2
Karan Sikka <karanssikka@gmail.com>
2025-10-02 13:40:16 -0400

    Make proj info multipage form, Add keypoint names to project info

--- web_ui/src/app/project-settings/project-settings.component.ts
+++ web_ui/src/app/project-settings/project-settings.component.ts
@@ -10,10 +10,11 @@ import {
 import { ProjectInfoService } from '../project-info.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ProjectInfo } from '../project-info';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-project-settings',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './project-settings.component.html',
   styleUrl: './project-settings.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,16 +23,19 @@ export class ProjectSettingsComponent implements OnInit {
   done = output<null>();
   setupMode = input(false);
 
+  protected selectedTab = signal<string>('directories');
   protected projectInfoForm: FormGroup;
   protected viewsInitialRows = signal(4);
+  protected keypointInitialRows = signal(4);
   private projectInfoService = inject(ProjectInfoService);
   private fb = inject(FormBuilder);
 
   constructor() {
     this.projectInfoForm = this.fb.group({
-      dataDir: [''],
-      modelDir: [''],
+      dataDir: '',
+      modelDir: '',
       views: [''],
+      keypointNames: [''],
     });
   }
 
@@ -42,35 +46,74 @@ export class ProjectSettingsComponent implements OnInit {
         dataDir: projectInfo.data_dir,
         modelDir: projectInfo.model_dir,
         views: projectInfo.views.join('\n'),
+        keypointNames: projectInfo.keypoint_names.join('\n'),
       });
+      this.projectInfoForm.markAsPristine();
+
       this.viewsInitialRows.update((x) =>
         Math.max(x, projectInfo.views.length),
+      );
+
+      this.keypointInitialRows.update((x) =>
+        Math.max(x, projectInfo.keypoint_names.length),
       );
     }
   }
 
   protected async onSaveClick() {
+    this.saveSuccessMessage.set('');
+    if (this.saveSuccessMessageClearTimerId) {
+      clearInterval(this.saveSuccessMessageClearTimerId);
+      this.saveSuccessMessageClearTimerId = 0;
+    }
+
     // Save the project info.
     const projectInfo = {} as Partial<ProjectInfo>;
+    if (this.projectInfoForm.get('dataDir')?.dirty) {
+      projectInfo.data_dir = this.projectInfoForm.get('dataDir')?.value ?? '';
+    }
 
-    projectInfo.data_dir = this.projectInfoForm.get('dataDir')?.value ?? '';
-    projectInfo.model_dir = this.projectInfoForm.get('modelDir')?.value ?? '';
-    projectInfo.views = this.parseMultilineText(
-      this.projectInfoForm.get('views')?.value ?? '',
-    );
+    if (this.projectInfoForm.get('modelDir')?.dirty) {
+      projectInfo.model_dir = this.projectInfoForm.get('modelDir')?.value ?? '';
+    }
+
+    if (this.projectInfoForm.get('views')?.dirty) {
+      projectInfo.views = this.parseTextAsList(
+        this.projectInfoForm.get('views')?.value ?? '',
+      );
+    }
+
+    if (this.projectInfoForm.get('keypointNames')?.dirty) {
+      projectInfo.keypoint_names = this.parseTextAsList(
+        this.projectInfoForm.get('keypointNames')?.value ?? '',
+      );
+    }
     await this.projectInfoService.setProjectInfo(projectInfo);
     // If successful reload the app because project info is global state.
-    window.location.reload();
+    this.saveSuccessMessage.set(
+      'Saved. Refresh the page for changes to take effect.',
+    );
+    // @ts-ignore
+    this.saveSuccessMessageClearTimerId = setTimeout(() => {
+      this.saveSuccessMessage.set('');
+      this.saveSuccessMessageClearTimerId = 0;
+    }, 5000) as number;
   }
 
-  private parseMultilineText(text: string): string[] {
+  protected parseTextAsList(text: string): string[] {
     return text
-      .split('\n')
-      .map((x) => x.trim())
-      .filter((x) => Boolean(x));
+      .replace(/[^\w\-]/g, ' ')
+      .split(/\s+/)
+      .filter(Boolean);
   }
 
   protected readonly cameraViewPlaceholder = `view1
 view2
 ...`;
+  protected saveSuccessMessage = signal<string>('');
+  private saveSuccessMessageClearTimerId = 0;
+
+  protected handleTabClick(tabKey: string) {
+    this.selectedTab.set(tabKey);
+  }
 }
