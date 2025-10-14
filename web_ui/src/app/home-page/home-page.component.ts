306ecd12b2856e2f42e9f2280d0fdbaff39e42e0
Karan Sikka <karanssikka@gmail.com>
2025-10-14 13:57:32 -0400

    fix menu order, and sessions getting clipped in extract frames

--- web_ui/src/app/home-page/home-page.component.ts
+++ web_ui/src/app/home-page/home-page.component.ts
@@ -7,4 +7,35 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './home-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  cards = [
+    {
+      href: '/',
+      title: 'Project settings',
+      description: 'View and edit configuration for the current Project',
+      imgSrc: 'https://img.daisyui.com/images/components/accordion.webp',
+      imgAlt: 'Accordion',
+    },
+    {
+      href: '/labeler',
+      title: 'Labeler',
+      description: 'View labeled data and label new frames',
+      imgSrc: 'https://img.daisyui.com/images/components/accordion.webp',
+      imgAlt: 'Accordion',
+    },
+    {
+      href: '/models',
+      title: 'Models',
+      description: 'Train, evaluate, and run models',
+      imgSrc: 'https://img.daisyui.com/images/components/accordion.webp',
+      imgAlt: 'Accordion',
+    },
+    {
+      href: '/viewer/',
+      title: 'Viewer',
+      description: 'View sessions and model predictions',
+      imgSrc: 'https://img.daisyui.com/images/components/accordion.webp',
+      imgAlt: 'Accordion',
+    },
+  ];
+}
