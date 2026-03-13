d3c262484f57b79f86441325476929f9f13fc7a5
Karan Sikka <karanssikka@gmail.com>
2026-03-13 16:46:29 -0400

    add viewer localstorage persistence

--- web_ui/src/app/viewer/viewer-view-options.service.ts
+++ web_ui/src/app/viewer/viewer-view-options.service.ts
@@ -1,11 +1,65 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal, computed, effect } from '@angular/core';
 import { ColorService } from '../infra/color.service';
+
+const DEFAULT_VIDEO_TILE_SIZE = 250;
+const DEFAULT_OPACITY = 1.0;
 
 @Injectable()
 export class ViewerViewOptionsService {
   colorService = inject(ColorService);
-  videoTileSizePx = signal<number>(250);
-  keypointOpacity = signal<number>(1.0);
-  keypointSize = signal<number>(this.colorService.defaultSize);
+
+  videoTileSizePx = signal<number>(
+    Number(localStorage.getItem('viewer-view-options.video-tile-size')) ||
+      DEFAULT_VIDEO_TILE_SIZE,
+  );
+  keypointOpacity = signal<number>(
+    Number(localStorage.getItem('viewer-view-options.opacity')) ||
+      DEFAULT_OPACITY,
+  );
+  keypointSize = signal<number>(
+    Number(localStorage.getItem('viewer-view-options.size')) ||
+      this.colorService.defaultSize,
+  );
   enableKeypointLabels = signal(false);
+
+  isVideoTileSizeDefault = computed(
+    () => this.videoTileSizePx() === DEFAULT_VIDEO_TILE_SIZE,
+  );
+  isOpacityDefault = computed(() => this.keypointOpacity() === DEFAULT_OPACITY);
+  isSizeDefault = computed(
+    () => this.keypointSize() === this.colorService.defaultSize,
+  );
+
+  constructor() {
+    effect(() =>
+      localStorage.setItem(
+        'viewer-view-options.video-tile-size',
+        String(this.videoTileSizePx()),
+      ),
+    );
+    effect(() =>
+      localStorage.setItem(
+        'viewer-view-options.opacity',
+        String(this.keypointOpacity()),
+      ),
+    );
+    effect(() =>
+      localStorage.setItem(
+        'viewer-view-options.size',
+        String(this.keypointSize()),
+      ),
+    );
+  }
+
+  resetVideoTileSize() {
+    this.videoTileSizePx.set(DEFAULT_VIDEO_TILE_SIZE);
+  }
+
+  resetOpacity() {
+    this.keypointOpacity.set(DEFAULT_OPACITY);
+  }
+
+  resetKeypointSize() {
+    this.keypointSize.set(this.colorService.defaultSize);
+  }
 }
