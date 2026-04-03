aaa51caf3b18e9653d973bcc15dc620ef395b960
Karan Sikka <karanssikka@gmail.com>
2026-04-03 12:48:21 -0400

    improve data directory user experience: home dir, populated by default

--- web_ui/src/app/project-settings/project-settings.component.ts
+++ web_ui/src/app/project-settings/project-settings.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectorRef,
   Component,
   computed,
+  DestroyRef,
   inject,
   input,
   OnInit,
@@ -19,6 +20,7 @@ import {
 import { ProjectInfo } from '../project-info';
 import { JsonPipe, NgTemplateOutlet } from '@angular/common';
 import { ValidatedPathInputComponent } from '../components/validated-path-input/validated-path-input.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-project-settings',
@@ -48,6 +50,7 @@ export class ProjectSettingsComponent implements OnInit {
   private projectInfoService = inject(ProjectInfoService);
   private fb = inject(FormBuilder);
   private cdr = inject(ChangeDetectorRef); // Keep cdr for markAllAsTouched
+  private destroyRef = inject(DestroyRef);
 
   // Protected instance fields for nested form groups
   protected directoriesForm!: FormGroup;
@@ -109,12 +112,31 @@ export class ProjectSettingsComponent implements OnInit {
   ngOnInit() {
     // Conditionally apply Validators.required to projectKey if in setup mode
     const projectKeyControl = this.directoriesForm.get('projectKey');
+    const defaultDataDir =
+      this.projectInfoService.globalContext()?.homeDir + '/LPProjects';
     if (this.setupMode()) {
+      const dataDirControl = this.directoriesForm.get('dataDir');
+      dataDirControl?.setValue(defaultDataDir);
+
       projectKeyControl?.setValidators(Validators.required);
     } else {
       projectKeyControl?.clearValidators();
     }
     projectKeyControl?.updateValueAndValidity();
+
+    if (this.setupMode()) {
+      projectKeyControl?.valueChanges
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe((projectKey) => {
+          const dataDirControl = this.directoriesForm.get('dataDir');
+          if (!dataDirControl?.dirty) {
+            const sanitizedKey = projectKey.replace(/ /g, '_');
+            dataDirControl?.setValue(`${defaultDataDir}/${sanitizedKey}`, {
+              emitEvent: true,
+            });
+          }
+        });
+    }
 
     // Init the form with existing project settings from server.
     if (!this.setupMode()) {
